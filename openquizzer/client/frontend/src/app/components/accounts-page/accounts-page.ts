cf0ba45adc9cd17cb2efa8cee0eaecf30aab93f5
Vraj2004 <vrajr.shah@mail.utoronto.ca>
2025-07-23 00:13:14 -0400

    Added functionality and styling to the navbar component

---
+++ openquizzer/client/frontend/src/app/components/accounts-page/accounts-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts-page',
+  imports: [],
+  templateUrl: './accounts-page.html',
+  styleUrl: './accounts-page.scss'
+})
+export class AccountsPage {
+
+}
