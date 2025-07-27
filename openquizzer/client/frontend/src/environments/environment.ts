a82b1a7c4aa44c201238d00b73dfdc6efe35bfee
Vraj2004 <vrajr.shah@mail.utoronto.ca>
2025-07-27 18:53:24 -0400

    Added dokcer compose

--- openquizzer/client/frontend/src/environments/environment.ts
+++ openquizzer/client/frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   stripePublishableKey: 'pk_test_51RgWgZFN6emTzMixVAdccRqitOHK1jFVNxt9VXEDRnOyWsGDX0bitBM5V10WKgkkYCowQfXBK6NLo9RO7YHAoHpt00Iea6KPst',
-  apiBaseUrl: 'http://localhost:3000/api'
+  apiBaseUrl: 'http://143.198.43.88:3000/api'
 };
