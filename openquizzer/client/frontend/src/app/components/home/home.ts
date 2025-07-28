e910d6bcf4c38dc4c3210b9d28924510d3f6a5e5
Arhum Parvez <arhum.parvez@mail.utoronto.ca>
2025-07-27 20:05:12 -0400

    fixed logout button and connect it with signout navbar button

--- openquizzer/client/frontend/src/app/components/home/home.ts
+++ openquizzer/client/frontend/src/app/components/home/home.ts
@@ -2,16 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Navbar } from '../navbar/navbar';
-import { HttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, Navbar],
+  imports: [CommonModule, Navbar,],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-  constructor(private router: Router, private http: HttpClient) {}
+  constructor(private router: Router) {}
 
   navigateToQuizMaster(): void {
     this.router.navigate(['/quiz-master']);
@@ -19,27 +17,5 @@ export class Home {
 
   navigateToQuizTaker(): void {
     this.router.navigate(['/quiz-taker']);
-  }
-
-  logout(): void {
-    const token = localStorage.getItem('token'); 
-    if (!token)
-      return;
-    const headers = {
-      Authorization: `Bearer ${token}`
-    };
-       lastValueFrom(
-        this.http.post<{message:string }>(
-          'http://localhost:3000/api/auth/logout',
-          {},
-          {headers}
-        )
-      )
-      .then((res) => {
-        console.log(res.message);
-      
-      localStorage.removeItem('token');
-      this.router.navigate(['/login']);
-    });
   }
 }
--- openquizzer/client/frontend/src/app/components/navbar/navbar.ts
+++ openquizzer/client/frontend/src/app/components/navbar/navbar.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { Router, RouterModule } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-navbar',
   imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+  logout(): void {
+    const token = localStorage.getItem('token'); 
+    if (!token)
+      return;
+    const headers = {
+      Authorization: `Bearer ${token}`
+    };
+       lastValueFrom(
+        this.http.post<{message:string }>(
+          'http://localhost:3000/api/auth/logout',
+          {},
+          {headers}
+        )
+      )
+      .then((res) => {
+        console.log(res.message);
+      
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    });
+  }
+}
+
+
