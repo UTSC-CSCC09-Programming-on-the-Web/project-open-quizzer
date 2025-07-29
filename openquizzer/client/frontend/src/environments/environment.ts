63012d140c4d5a396556410f728652c87449707c
Vraj2004 <vrajr.shah@mail.utoronto.ca>
2025-07-29 01:48:33 -0400

    Actual vm ip

--- openquizzer/client/frontend/src/environments/environment.ts
+++ openquizzer/client/frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   stripePublishableKey:
     'pk_test_51RgWgZFN6emTzMixVAdccRqitOHK1jFVNxt9VXEDRnOyWsGDX0bitBM5V10WKgkkYCowQfXBK6NLo9RO7YHAoHpt00Iea6KPst',
-  apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'http://143.198.43.88/api',
 };
