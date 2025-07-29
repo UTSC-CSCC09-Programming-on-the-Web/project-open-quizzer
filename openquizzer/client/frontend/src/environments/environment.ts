2b390669c8a9805adff0528e71585636bed00c2b
HardikNarkhede <hardikn2010@gmail.com>
2025-07-29 15:30:46 -0400

    updated environment.ts to also rely on nginx too

--- openquizzer/client/frontend/src/environments/environment.ts
+++ openquizzer/client/frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   stripePublishableKey:
     'pk_test_51RgWgZFN6emTzMixVAdccRqitOHK1jFVNxt9VXEDRnOyWsGDX0bitBM5V10WKgkkYCowQfXBK6NLo9RO7YHAoHpt00Iea6KPst',
-  apiBaseUrl: 'http://143.198.43.88/api',
+  apiBaseUrl: '/api',
 };
