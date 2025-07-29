3d7ba794ade7a1f793c9689e4a1f9fde15f14928
HardikNarkhede <hardikn2010@gmail.com>
2025-07-29 14:51:35 -0400

    Altered environment.prod.ts to use nginx proxy for apiBaseUrl

--- openquizzer/client/frontend/src/environments/environment.prod.ts
+++ openquizzer/client/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  stripePublishableKey:
-    'pk_test_51RgWgZFN6emTzMixVAdccRqitOHK1jFVNxt9VXEDRnOyWsGDX0bitBM5V10WKgkkYCowQfXBK6NLo9RO7YHAoHpt00Iea6KPst',
-  apiBaseUrl: 'http://143.198.43.88:3000/api',
+  apiBaseUrl: '/api', // nginx proxy handles this
 };
