4ddd6532c91aa0e1d87fe1eba1668a28649e814f
HardikNarkhede <hardikn2010@gmail.com>
2025-07-29 22:49:53 -0400

    Uncommented requrie statment for stripe

--- openquizzer/server/controllers/subscriptionController.js
+++ openquizzer/server/controllers/subscriptionController.js
@@ -1,4 +1,4 @@
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const subscriptionService = require("../services/subscriptionService");
 const webhookSecret = process.env.STRIPE_WEBHOOK_KEY;
 
